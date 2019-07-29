dc7af0aad4979cefae40d1cbcf8fef5efc669f88
rsd1244 <50500017+rsd1244@users.noreply.github.com>
2019-07-28 23:30:16 -0400

    Create mountain_poll.py

---
+++ mountain_poll.py
@@ -0,0 +1,22 @@
+responses = {}
+
+#Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    #Prompt the user for a Name and a response.
+    name = input('\nWhat is your name? ')
+    response = input('\nWhat mountain do you want to climb? ')
+
+    #Store the inputs in a dictionary
+    responses[name] = response
+
+    #Find out if anyone else will be taking the poll.
+    repeat = input('Would you like anyone else to take the poll? (yes|no) ')
+    if repeat == 'no':
+        polling_active = False
+
+#Polling is complete. Show the results of the poll.
+print('\n--- Poll Results ---')
+for name, response in responses.items()
+    print(name, 'would like to climb', response + '.')
