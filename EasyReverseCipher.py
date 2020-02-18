dd174bb8158674b6e055bc8f3934a486e9fbb9d0
rsd1244 <50500017+rsd1244@users.noreply.github.com>
2020-02-17 20:31:40 -0500

    Create EasyReverseCipher.py

---
+++ EasyReverseCipher.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+'''
+This program does some stuff
+Author: Ritchie Deel
+'''
+message = input("Enter the text to be encrypted here: ")
+translated = message[::-1]
+print(translated)
+
