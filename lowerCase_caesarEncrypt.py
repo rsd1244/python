9896bdba654f4dddc4229c1e783060bed467ee09
rsd1244 <50500017+rsd1244@users.noreply.github.com>
2020-02-17 20:24:52 -0500

    Create lowerCase_caesarEncrypt.py

---
+++ lowerCase_caesarEncrypt.py
@@ -0,0 +1,13 @@
+
+#!/bin/usr/env python3
+plainText = input("Enter a one-word, lowercase message to be encrypted: ")
+distance = int(input("Enter the distance value: "))
+code = ""
+for ch in plainText:
+    ordValue = ord(ch)
+    cipherValue = ordValue + distance
+    if cipherValue > ord('z'):
+        cipherValue = ord('a') + distance - (ord('z') - ordValue + 1)
+    code += chr(cipherValue)
+print(code)
+
